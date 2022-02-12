e65523812141fbec620d38cb132c8cf2b99c52bc
muklis <sasantomuklis@gmail.com>
2022-02-12 14:32:24 +0700

    Revert "ubah 1-pointer.go"
    This reverts commit 1a10473394d8c0b0c91b10a70f9bfc93b8766b29.

--- chapter-A.23-pointer/1-pointer.go
+++ chapter-A.23-pointer/1-pointer.go
@@ -1,28 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 func main() {
 	var numberA int = 4
 	var numberB *int = &numberA
-	var numberC *int = numberB
-	var numberD **int = &numberB
-
-	var reflectValue = reflect.ValueOf(numberD)
-	fmt.Println("tipe  variabel :", reflectValue.Type())
+	var numberC = &numberB
 
 	fmt.Println("numberA (value)   :", numberA)  // 4
-	fmt.Println("numberA (address) :", &numberA) // 0xc0000140a8
+	fmt.Println("numberA (address) :", &numberA) // 0xc20800a220
 
 	fmt.Println("numberB (value)   :", *numberB) // 4
-	fmt.Println("numberB (address) :", numberB)  // 0xc0000140a8
+	fmt.Println("numberB (address) :", numberB)  // 0xc20800a220
 
-	fmt.Println("numberC (value)   :", *numberC) // 4
-	fmt.Println("numberC (address) :", numberC)  // 0xc0000140a8
-
-	fmt.Println("numberD (value)   :", *numberD) // 0xc0000140a8
-	fmt.Println("numberD (address) :", numberD)  // 0xc00000e028
+	fmt.Println("numberC :", numberC)
+	fmt.Println("numberC :", *numberC)
 }
