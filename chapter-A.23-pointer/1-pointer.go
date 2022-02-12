1a10473394d8c0b0c91b10a70f9bfc93b8766b29
muklis <sasantomuklis@gmail.com>
2022-02-12 14:05:37 +0700

    ubah 1-pointer.go

--- chapter-A.23-pointer/1-pointer.go
+++ chapter-A.23-pointer/1-pointer.go
@@ -1,18 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	var numberA int = 4
 	var numberB *int = &numberA
-	var numberC = &numberB
+	var numberC *int = numberB
+	var numberD **int = &numberB
+
+	var reflectValue = reflect.ValueOf(numberD)
+	fmt.Println("tipe  variabel :", reflectValue.Type())
 
 	fmt.Println("numberA (value)   :", numberA)  // 4
-	fmt.Println("numberA (address) :", &numberA) // 0xc20800a220
+	fmt.Println("numberA (address) :", &numberA) // 0xc0000140a8
 
 	fmt.Println("numberB (value)   :", *numberB) // 4
-	fmt.Println("numberB (address) :", numberB)  // 0xc20800a220
+	fmt.Println("numberB (address) :", numberB)  // 0xc0000140a8
 
-	fmt.Println("numberC :", numberC)
-	fmt.Println("numberC :", *numberC)
+	fmt.Println("numberC (value)   :", *numberC) // 4
+	fmt.Println("numberC (address) :", numberC)  // 0xc0000140a8
+
+	fmt.Println("numberD (value)   :", *numberD) // 0xc0000140a8
+	fmt.Println("numberD (address) :", numberD)  // 0xc00000e028
 }
